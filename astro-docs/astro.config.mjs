e9fa1a24438a3b607823ed3c20684d71f128c9c1
Zaynity <zaynity.gamer@gmail.com>
2025-05-31 20:40:10 +0200

    UPD: Add actions & kub documentation to navigation menu

--- astro-docs/astro.config.mjs
+++ astro-docs/astro.config.mjs
@@ -10,10 +10,11 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Docs',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'GitHub Actions', slug: 'infra/github_actions' },
+						{ label: 'Kubernetes', slug: 'infra/kubernetes' },
 					],
 				},
 				{
